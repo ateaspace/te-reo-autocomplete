f8665bcea49dba1ce2f4c4688e1e6b8e989021e7
Finn Innes <finn123890@gmail.com>
2022-02-02 13:44:08 +1300

    removed bigram tree generation due to performance impact

--- java-autocomplete/src/main/java/org/atea/nlptools/autocomplete/PlainText.java
+++ java-autocomplete/src/main/java/org/atea/nlptools/autocomplete/PlainText.java
@@ -6,10 +6,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.StringTokenizer;
 import opennlp.tools.sentdetect.*;
 import packages.prt.PruningRadixTrie;
-import packages.prt.TextSanitized;
 
 public class PlainText extends PATServlet {
 
@@ -54,7 +52,7 @@ public class PlainText extends PATServlet {
                 while ((row = inputReader.readLine()) != null) {
                     row = row.trim();
                     if (!row.equals("") && !row.isEmpty()) {
-                        if (row.length() < 150) { // if row contains less than 500 chars, run OpenNLP, otherwise regex split
+                        if (row.length() < 150) { // if row contains less than x chars, run OpenNLP, otherwise regex split
                             splitMsg = detector.sentDetect(row);
                         } else {
                             splitMsg = row.split(MPTR_SPLIT);
@@ -89,46 +87,10 @@ public class PlainText extends PATServlet {
                 printError("ERROR: " + e.getMessage());
                 e.printStackTrace();
             }
-
-            try { // Bigram PRT indexing
-                BufferedReader inputReader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8)); // create reader interface with UTF-8 encoding for macron support
-                String row;
-    
-                // while ((row = inputReader.readLine()) != null) {
-                //     // tokenizer to maintain word position within sentence
-                //     StringTokenizer itr = new StringTokenizer(row.toLowerCase().trim().replace("\"", ""));
-                //     if (itr.countTokens() > 1) {
-                //         String s1 = "";
-                //         String s2 = "";
-                //         while (itr.hasMoreTokens())
-                //         {
-                //             if (s1.isEmpty())
-                //                 s1 = itr.nextToken();
-                //             s2 = itr.nextToken();
-                //             String termOriginal = s1 + " " + s2;
-                //             TextSanitized term = new TextSanitized(termOriginal);
-                //             currBigramPRT.addTerm(term, 1); // add words to PRT and increment count
-                //             s1 = s2;
-                //             s2 = "";
-                //         }
-                //     }
-                // }
-
-                TextSanitized term = new TextSanitized("-");
-                currBigramPRT.addTerm(term, 1);
-
-                inputReader.close();
-                System.out.println(String.format("%,d", (int)currBigramPRT.termCount) + " bigram terms written from " + file);
-
-                if (serType == serializeType.text) {
-                    writeTextSerializedFile(MPTR_EXPORT, BIGRAM_EXPORT);
-                } else if (serType == serializeType.binary) {
-                    writeBinarySerializedFile(MPTR_SER, BIGRAM_EXPORT);
-                }
-            }
-            catch (Exception e) {
-                printError("ERROR: " + e.getMessage());
-                e.printStackTrace();
+            if (serType == serializeType.text) {
+                writeTextSerializedFile(MPTR_EXPORT, BIGRAM_EXPORT);
+            } else if (serType == serializeType.binary) {
+                writeBinarySerializedFile(MPTR_SER, BIGRAM_EXPORT);
             }
         }
     }
