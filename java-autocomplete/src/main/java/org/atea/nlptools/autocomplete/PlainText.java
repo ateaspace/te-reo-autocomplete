eed68c4c4e35a408082cd394e73b915463b7f259
Finn Innes <finn123890@gmail.com>
2022-02-03 13:13:55 +1300

    re-introduced bigram build - memory exception no longer present, probably due to long sentence removal

--- java-autocomplete/src/main/java/org/atea/nlptools/autocomplete/PlainText.java
+++ java-autocomplete/src/main/java/org/atea/nlptools/autocomplete/PlainText.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
+
 import opennlp.tools.sentdetect.*;
+import packages.prt.TextSanitized;
 
 public class PlainText extends PATServlet {
 
@@ -39,15 +43,9 @@ public class PlainText extends PATServlet {
                             } 
                         }
                         lineCount++;
-                        // if (lineCount > 200000) {
-                        //     break;
-                        // }
                         if (lineCount % 1000 == 0) {
                             System.out.println("processing line: " + trueLineCount + " line length: " + row.length());
                         }
-                        // if (lineCount > 195000) {
-                        //     System.out.println("processing line: " + trueLineCount + " line length: " + row.length());
-                        // }
                     }
                     trueLineCount++;
                 }
@@ -58,6 +56,35 @@ public class PlainText extends PATServlet {
                 System.out.println("===== build time: " + elapsedTime + "ms ===== ");            }
             catch (Exception e) {
                 printError("ERROR: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+            try {
+                BufferedReader inputReader;
+                inputReader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8));
+                String row;
+
+                while ((row = inputReader.readLine()) != null) {
+                    // tokenizer to maintain word position within sentence
+                    StringTokenizer itr = new StringTokenizer(row.toLowerCase().trim().replace("\"", ""));
+                    if (itr.countTokens() > 1) {
+                        String s1 = "";
+                        String s2 = "";
+                        while (itr.hasMoreTokens())
+                        {
+                            if (s1.isEmpty())
+                                s1 = itr.nextToken();
+                            s2 = itr.nextToken();
+                            String termOriginal = s1 + " " + s2;
+                            TextSanitized term = new TextSanitized(termOriginal);
+                            currBigramPRT.addTerm(term, 1); // add words to PRT and increment count
+                            s1 = s2;
+                            s2 = "";
+                        }
+                    }
+                }
+                inputReader.close();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             writeSerialized();
